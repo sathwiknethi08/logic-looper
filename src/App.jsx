b495bbe37e668155c1e9eab9027c077d4b0ca26d
Sathwik Nethi <sathwiknethi51@gmail.com>
2026-02-10 23:19:41 +0530

    Logic Looper MVP

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import PuzzleBoard from "./PuzzleBoard";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>🧠 Logic Looper</h1>
+      <PuzzleBoard />
+    </div>
+  );
+}
